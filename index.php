85ff0b2fd5ffdd548cecdc6a51e5199bd93ce00f
Singh57 <rsumra57@gmail.com>
2017-03-26 20:21:03 +0930

    Version2

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+$title = "Home";
+$content = '
+        <img src="Images/cdu logo.jpg" class="imgLeft" />
+        <h3>Parameterized Complexity</h3>
+        <p>
+            Parameterized Complexity is a recent branch of computational complexity theory that provides
+            a framework for a refined analysis of hard algorithmic problems. The big, important problems
+            facing Planet Earth have structure with “secondary” measurements (parameters), apart from 
+            the primary measurement of overall input size that significantly affect problem computational 
+            complexity.
+            The central notion of fixed parameter tractability (FPT) is a generalization of polynomial-time 
+            based on confining any non-polynomial (typically exponential) complexity costs to a function 
+            only of these secondary measurements
+        </p>
+
+        <img src="Images/coffee2.png" class="imgRight" />
+        <h3>Who We are ?</h3>
+        <p>
+            The Parameterized Complexity Research Unit is an independent research unit in the Charles Darwin
+            University. Established in 2012,PCRU interacts with a broad international research community 
+            and undertakes research that is strongly interdisciplinary.
+         </p>
+
+         <img src="Images/coffee3.png" class="imgLeft" />
+         <h3>Multivariate Algorithmics</h3>
+         <p>
+           The big, important problems facing Planet Earth have structure with secondary measurements (parameters),
+           apart from the primary measurement of overall input size, that significantly affect problem computational
+           complexity.Parameterized Complexity is a recent branch of computational complexity theory that provides a 
+           framework for a refined analysis of hard algorithmic problems.The multivariate approach allows more realistic 
+           modelling of real-world input distributions.
+         </p>';
+include 'Template.php';
+?>
